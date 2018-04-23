82b3616f1245ec2318d07e46a1f0da4283de355b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+
+Imports DevExpress.XtraReports.Web
+Imports DevExpress.XtraReports.UI
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            If Session("Report") Is Nothing Then
+                Dim r As New XtraReport1()
+                Session("Report") = r
+            End If
+            ASPxDocumentViewer1.Report = TryCast(Session("Report"), XtraReport)
+    End Sub
+    Protected Sub ReportViewer1_Unload(ByVal sender As Object, ByVal e As EventArgs)
+        DirectCast(sender, ASPxDocumentViewer).Report = Nothing
+    End Sub
+End Class
